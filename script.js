ad4869ed0642ced059ce856dd7a353cd56d1aa62
Dominik Stasiak <d.stasiak436@gmail.com>
2023-09-14 11:11:44 +0200

    created basic version of the program

---
+++ script.js
@@ -0,0 +1,27 @@
+function create(size) {
+    let content = document.querySelector(".content");
+    let squares = content.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    content.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    content.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size*size;
+    for(let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = "white";
+        content.insertAdjacentElement("beforeend", square);
+        square.addEventListener('mouseover', ()=> {
+            square.style.backgroundColor = "black"}
+            );
+}}
+
+create(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        create(input);
+    }
+    else {
+        alert("Invalid number of squares");
+    }
+}
